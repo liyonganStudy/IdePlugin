9e03034c6320778205944b4ebdce04568e5bf1e2
GitHub Action <action@github.com>
2020-12-09 07:02:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/liyonganstudy/ideplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/liyonganstudy/ideplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.liyonganstudy.ideplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.liyonganstudy.ideplugin.MyBundle
 
 class MyApplicationService {
 
